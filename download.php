1a0e92d7bee57f26799a51930dc0a51e5ce2184a
Hairul Lana <hairullana99@gmail.com>
2021-11-03 14:30:57 +0800

    separate headtags and change redirect download page

--- download.php
+++ download.php
@@ -1,53 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-709XQRCR4T"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-709XQRCR4T');
-    </script>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="Samudra Learning Center" />
-    <meta name="author" content="Squad FCC0220 POM" />
+    <?php require 'header.php' ?>
     <title>Download SAVILE</title>
-    <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.png" />
-
-    <!-- Font Awesome icons (free version)-->
-    <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
-    <!-- Google fonts-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <!-- CSS only -->
-    <link href="css/styles.css" rel="stylesheet" />
 </head>
 <body>
-    <div class="text-center mt-5">
-        <h3>
-            You will be redirected to a download page in <span>3</span> seconds           
-        </h3>
-    </div>
-    <script type="text/javascript">   
-        function Redirect() {  
-            window.location="https://www.google.com"; 
-        } 
-        let time = 3000
-        setTimeout('Redirect()', time);
-        
-        </script>
+    <div id="pesan" class="mt-5 text-center"></div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>
+        var url = "https://google.com"; // url tujuan
+        var count = 5; // dalam detik
+        function countDown() {
+            if (count > 0) {
+                count--;
+                var waktu = count + 1;
+                $('#pesan').html('<h3>Kamu akan dialihkan ke halaman download setelah <font size=5 color=maroon>' + waktu + '</font> detik.</h1>');
+                setTimeout("countDown()", 1000);
+            } else {
+                window.location.href = url;
+            }
+        }
+        countDown();
+    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Core theme JS-->
+    <!-- Core theme JS-->Z
     <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
     <script src="js/scripts.js"></script>
 </body>
--- index.php
+++ index.php
@@ -1,36 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-709XQRCR4T"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-709XQRCR4T');
-        </script>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="Samudra Learning Center" />
-        <meta name="author" content="Squad FCC0220 POM" />
+        <?php require 'header.php' ?>
         <title>SAVILE - Samudra Virtual Learning</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.png" />
-        <!-- AOS CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
-
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <!-- CSS only -->
-        <link href="css/styles.css" rel="stylesheet" />
     </head>
     <body id="page-top">
         <!-- Navigation-->
